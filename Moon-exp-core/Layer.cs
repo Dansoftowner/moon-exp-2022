1bc39091ed59d27e86ed43a644d21a4faaab4c31
Daniel Gyorffy <judalion777@gmail.com>
2022-03-09 13:25:54 +0100

    :construction: Preparations

---
+++ Moon-exp-core/Layer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace moon_exp_core
+{
+    class Layer
+    {
+
+        public List<Lens> lenses;
+        private Dictionary<int, int> measurements;
+
+        public bool ThinnedCompletely_6 => false;
+        public int PossibleLensesCount_8 => 0;
+        public int MaxThickness_5 => 0;
+        public int MeasurementsCount => 0;
+
+        public Layer(string measurements)
+        {
+
+        }
+    }
+}
